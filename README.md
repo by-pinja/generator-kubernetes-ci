064f78884645f1074efc2b808b716a38546da22c
Savpek <savolainen.pekka@gmail.com>
2017-09-27 07:33:10 +0300

    Refactored structure and added other files except k8s.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 1. Create repository to phabricator.
 2. Clone repository.
 3. Install yeoman `npm install -g yo`
-4. Run in new repository `yo @ptcs/yo-k8s-template`
-5. Commit and push changes, now CI pipeline builds project, publishes containers and deploy k8s files. After CI is complete you can navigate `https://project-name.protacon.cloud`. Project name is asked after you run yo.
+4. Run in new repository `yo @protacon/kubernetes-ci`
+5. Commit and push changes, now CI pipeline builds project, publishes containers and deploy k8s files. After CI is complete you can navigate `https://project-name-branch.protacon.cloud`. Project name is asked after you run yo.
---
+++ generators/app/index.js
@@ -0,0 +1,83 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the wicked ' + chalk.red('@protacon/kubernetes-ci') + ' generator!'
+    ));
+
+    const prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'Project name?',
+      default: this.appname
+    },
+    {
+      type: 'input',
+      name: 'namespace',
+      message: 'Kubernetest namespace (in test environment)?',
+      default: this.appname
+    },
+    {
+      type: 'input',
+      name: 'branch',
+      message: 'Current branch in git?',
+      default: 'master'
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('Dockerfile'),
+      this.destinationPath('Dockerfile'), {
+        projectName: this.props.projectName,
+        namespace: this.props.namespace
+      });
+
+    this.fs.copyTpl(
+      this.templatePath('Jenkinsfile'),
+      this.destinationPath('Jenkinsfile'), {
+        projectName: this.props.projectName,
+        namespace: this.props.namespace
+      });
+
+    this.fs.copyTpl(
+      this.templatePath('k8s/k8s.yaml'),
+      this.destinationPath('k8s/' + this.props.branch + '.yaml'), {
+        projectName: this.props.projectName,
+        namespace: this.props.namespace
+      });
+
+    this.fs.copy(
+      this.templatePath('nginx.conf'),
+      this.destinationPath('nginx.conf')
+    );
+
+    this.fs.copy(
+      this.templatePath('nginx.conf'),
+      this.destinationPath('nginx.conf')
+    );
+
+    this.fs.copy(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md')
+    );
+
+    this.fs.copy(
+      this.templatePath('exampleApp/index.html'),
+      this.destinationPath('exampleApp/index.html')
+    );
+  }
+
+  install() {
+  }
+};
